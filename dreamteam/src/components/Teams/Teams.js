74ce7e4c084f1c37f0a0f585398be3d063345e73
William Fitzgerald <willfitzy03@gmail.com>
2022-09-16 12:12:36 -0400

    got teams to show

--- dreamteam/src/components/Teams/Teams.js
+++ dreamteam/src/components/Teams/Teams.js
@@ -1,15 +1,32 @@
 import { useNavigate } from 'react-router'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const Teams = ({ team }) => {
-  // let navigate = useNavigate
+const Teams = (props) => {
+  const [teams, setTeams] = useState([])
+
+  let navigate = useNavigate
+
+  useEffect(() => {
+    const getTeams = async () => {
+      const res = await axios.get(`http://localhost:3001/api/team/all`)
+      setTeams(res.data)
+    }
+    getTeams()
+  }, [])
+
+  const handleDelete = async (id) => {
+    await axios.delete(`/team/${id}`)
+    console.log('banaana')
+    props.getItems()
+  }
 
   return (
-    <div className="latest-jobs-grid">
-      {team?.map((team) => (
+    <div className="team-grid">
+      {props.Teams?.map((team, index) => (
         <div className="team-card" key={team._id}>
-          <h1>hello</h1>
-          <h2>{team.name}</h2>
-          {/* <h3>players: {Team.salary}</h3> */}
+          <h3>{team.name}</h3>
+          <button onClick={() => handleDelete(team._id)}>Delete Team</button>
         </div>
       ))}
     </div>
